12757336baeef570950080f604093a8c8afdeea0
Dylan3009 <dylan.melendezl@hotmail.com>
2022-07-07 23:39:25 -0600

    Adding Authentication

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 var debug = require('debug')('ForGot:server');
 
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb+srv://mongodb-cacc75f9.mongo.ondigitalocean.com")
+  .connect(process.env.MONGO_URI || "mongodb+srv://doadmin:Lq371yA9uPm842M5@mongodb-cacc75f9.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=mongodb")
   .then(
     () => {
       debug("Database connected ");
